a702b477ced0afe99fb3315ddc6a1b958c2c85bf
makun-u <tor_a11um@icloud.com>
2019-09-14 21:53:15 +0900

    aws-commit

--- config/initializers/time_fomats.rb
+++
@@ -1,4 +0,0 @@
-Date::DATE_FORMATS[:default] = "%Y年%m月%d日".dup()
-Date::DATE_FORMATS[:date] = "%m/%d".dup()
-DateTime:DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分".dup()
-DateTime:DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M".dup()
